aac1450396f772f790babb390430e615ce68db64
Samantha <samccollinsfl@gmail.com>
2019-10-24 09:41:49 -0400

    Resubmission of shopper object

--- Week-1-Assignments/grocery-store-js/index.js
+++ Week-1-Assignments/grocery-store-js/index.js
@@ -1,13 +1,14 @@
 var shopper = {
-    firstName = "Samantha",
-    lastName = "Tackett",
-    age = 29,
-    groceryCart = ["Milk", "Eggs", "Bread", "Chicken"],
-    numberOfItems = 4,
-    lessThan5Items = true,
-    fullName = function () {
-        return this.firstName + " " + this.lastName;
+    firstName: 'Samantha',
+    lastName: 'Tackett',
+    age: 29,
+    groceryCart: ['Milk', 'Eggs', 'Bread', 'Chicken'],
+    numberOfItems: 4,
+    lessThan5Items: true,
+    fullName: function () {
+        console.log(this.firstName + ' ' + this.lastName)
     }
 }
 
-console.log(shopper)
+console.log(shopper);
+console.log(shopper.fullName());
